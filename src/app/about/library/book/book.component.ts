a769a585e85324c1e4275e918eb3d651f914b7b0
Shubham Lashkan <shubhamlashkan@gmail.com>
2019-11-18 22:11:20 +0530

    Library Book and Thesis Page

--- src/app/about/library/book/book.component.ts
+++ src/app/about/library/book/book.component.ts
@@ -11,83 +11,54 @@ import { ToastrManager } from 'ng6-toastr-notifications';
   providers : [LibraryService]
 })
 export class BookComponent implements OnInit {
-  length: any;
-  edit : boolean = false;
-  constructor(private library : LibraryService, private toastr :  ToastrManager) { }
-  @ViewChild(MatPaginator) paginator : MatPaginator;
-  @ViewChild('bookForm') bookForm : NgForm;
-    ngOnInit() {
-    this.library.getBooksCount()
-    .subscribe(
-      data =>{
-        this.length = data;
-      }
-    )
-    }
-  
-  displayedColumns: string[] = ['No.', 'Book name', 'Author', 'Status'];
-  onRowClicked(row) {
-    console.log(row);
-    this.edit=true;
-    this.bookForm.setValue({
-      'serialNo' : row.serialNo || null,
-      'bookNo' : row.bookNo || null,
-      'bookName' : row.bookName || null,
-      'author' : row.author || null,
-      'type' : row.type || null,
-      'registeredOn' : row.registeredOn || null,
-      'version' : row.version || null,
-      'status' : row.status || null,
-       'remarks' : row.remarks || null
-    })
-   
-}
-initForm():void{
-  this.edit=false;
-  this.bookForm.setValue({
-    'serialNo' : null,
-    'bookNo' : null,
-    'bookName' : null,
-    'author' : null,
-    'type' : null,
-    'registeredOn' :  null,
-    'version' : null,
-    'status' :  null,
-     'remarks' : null
-  })
+  userType : string = localStorage.getItem('userType');
+  student : boolean;
+  staff : boolean;
+  faculty : boolean;
+  hod : boolean;
  
-}
-editBook(f: NgForm):void{
- console.log(f.value);  
- this.library.editBookDetails(f.value)
- .subscribe(
-   data =>{
-      this.toastr.successToastr(data.message,'Success!');
-   },
-   error =>{
-     this.toastr.errorToastr("Error!!, try again after some time", 'Alert!')
-     console.log(error);
-   }
- )   
-}
- addBook(f : NgForm):void{
-  console.log(f.value);  
-  this.library.addBookDetails(f.value)
-  .subscribe(
-    data =>{
-       this.toastr.successToastr(data.message,'Success!');
-    },
-    error =>{
-      if(error.status != 504)
-      {
-        this.toastr.errorToastr(error.error.message, 'Alert!')  
-      }
-      else{
-        this.toastr.errorToastr("Error!!, try again after some time", 'Alert!')
-      }
-      
-      console.log(error);
+   constructor() {
+  }
+
+
+
+  completionMessage: string="Error has Occurred. Try after some time!!";
+  showConfirmation: boolean;
+  errorMessage: string = "Error has Occurred. Try after some time!!";
+
+   
+  ngOnInit() {
+    this.showConfirmation = false;
+  
+    this.student=false;
+    this.staff=false;
+    this.faculty=false;
+    this.hod=false;
+   
+
+    
+
+    
+    
+
+    if( this.userType === "student")
+    {
+      this.student = true;
     }
-  )
- }
+    if( this.userType === "staff"){
+      this.staff = true;
+    }
+    if( this.userType === "head"){
+      this.hod = true;
+    }
+    if(this.userType === "faculty")
+    {
+      this.faculty = true;
+    }
+ 
+  }
+  
 }
+  
+  
+
